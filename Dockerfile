77b47e1ad05b93e227fba61852b298ccdcd801a2
mustafakucuk-dev <mustfkck@gmail.com>
2022-02-24 22:48:26 +0300

    Tricky database endpoint change

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,5 @@ FROM python:3.7-slim
 RUN pip install flask
 WORKDIR /app
 COPY app.py /app/app.py
-ARG DB_HOST="localhost"
-ARG DB_PORT=6379
-ENV REDIS_HOST=${DB_HOST}
-ENV REDIS_PORT=${DB_PORT}
 ENTRYPOINT ["python"]
 CMD ["/app/app.py"]
--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import time
 import os
 import redis
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, session
+import socket
 
 app = Flask(__name__)
 
-redis_host = os.environ['REDIS_HOST']
-redis_port = os.environ['REDIS_PORT']
+#redis_host = os.environ['REDIS_HOST']
+#redis_port = os.environ['REDIS_PORT']
+#cache = redis.Redis(host=redis_host, port=redis_port)
+
+redis_host = "localhost"
+redis_port = 6379
 cache = redis.Redis(host=redis_host, port=redis_port)
 
 def get_hit_count():
@@ -24,3 +29,22 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/changeDatabase', methods=['POST'])
+def changeDB():
+    global redis_host, redis_port, cache
+    print(redis_host)
+    print(redis_port)
+    request_data = request.get_json()
+    redis_host = request_data["REDIS_HOST"]
+    redis_port = request_data["REDIS_PORT"]
+    test_db = socket.socket()
+    try:
+        test_db.connect((redis_host, int(redis_port)))
+        cache = redis.Redis(host=redis_host, port=redis_port)
+        return "DB Endpoint Changed To : " + str(redis_host)+ " : " + str(redis_port)
+    except Exception as e:
+        return "Something's wrong with given endpoint -> " + str(redis_host) + " : " + str(redis_port) + "\n Exception is " + str(e)
+    finally:
+        test_db.close()
+    
